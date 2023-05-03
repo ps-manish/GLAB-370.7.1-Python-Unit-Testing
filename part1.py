3be904d22cae8319ab1332568c751b8f50bf816e
ps-manish <126000688+ps-manish@users.noreply.github.com>
2023-05-03 09:34:39 -0600

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,12 @@
+import unittest
+
+def add_numbers(a, b):
+    return a + b
+
+class TestAddNumbers(unittest.TestCase):
+    def test_add_numbers(self):
+        result = add_numbers(3, 5)
+        self.assertEqual(result, 8)
+
+if __name__ == '__main__':
+    unittest.main()
